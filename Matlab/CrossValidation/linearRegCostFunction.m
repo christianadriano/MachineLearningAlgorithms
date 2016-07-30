1546ef9a389008a943b3569a8d190bf9c0c1af80
Christian Medeiros Adriano <adrianoc@uci.edu>
2016-07-29 22:46:34 -0700

    regularized linear regression

---
+++ Matlab/CrossValidation/linearRegCostFunction.m
@@ -0,0 +1,42 @@
+
+
+
+function [J, grad] = linearRegCostFunction(X, y, theta, lambda)
+%LINEARREGCOSTFUNCTION Compute cost and gradient for regularized linear 
+%regression with multiple variables
+%   [J, grad] = LINEARREGCOSTFUNCTION(X, y, theta, lambda) computes the 
+%   cost of using theta as the parameter for linear regression to fit the 
+%   data points in X and y. Returns the cost in J and the gradient in grad
+
+% Initialize some useful values
+m = length(y); % number of training examples
+
+% You need to return the following variables correctly 
+J = 0;
+grad = zeros(size(theta));
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Compute the cost and gradient of regularized linear 
+%               regression for a particular choice of theta.
+%
+%               You should set J to the cost and grad to the gradient.
+%
+
+hypothesis = X * theta;
+
+theta_modified = theta;
+theta_modified(1) = 0 ; %So I do not penalize the theta-zero
+theta_squared = sum(theta_modified.^2);
+regularization = theta_squared * lambda / (2*m);
+
+J = (( hypothesis - y )' * ( hypothesis - y )) /(2*m);
+
+J = J + regularization;
+
+% =========================================================================
+
+grad = (1/m)*X'*(hypothesis - y) + (lambda * theta_modified /m);
+
+
+
+end
